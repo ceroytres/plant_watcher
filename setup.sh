9261f0956eed22882c731d646f35fb1c236f320a
Diego <dmarez8ece@gmail.com>
2020-07-29 18:48:30 -0700

    add python3 check

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+#check python
+if [[ "$(python -c "import sys; print(sys.version[0])")" -ne "3" ]]; then
+    printf "Aliasing python3 as python"
+    alias python=python3
+fi
+
 
 python -m venv .venv
 
