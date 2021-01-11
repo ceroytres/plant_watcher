bcc6ca9e20f10ca64fd1c7a12cf0d78407405cec
ceroytres <ceroytres@users.noreply.github.com>
2021-01-10 21:32:11 -0800

    broke everything

---
+++ docs/lux_cal.md
@@ -0,0 +1,22 @@
+# Setup Simple Luxmeter
+
+1. Using a multimeter using measure the resistance across the photoresistor, and measure the correpsonding lux level using a luxmeter. Record values in a csv file with resistance being the first column, and lux being the second column as shown below:
+```
+R0,L0
+R1,L1
+```
+2. In order to create a fitted lux model
+```bash
+./tools/fit_lux_lr.py --input <input data path> --output <output model path> 
+```
+and optional flag maybe passed ```--robust``` which fits to the collected data using [Huber regression](https://scikit-learn.org/stable/modules/linear_model.html#huber-regression) instead of least squares. 
+
+### Resources
+* List luxmeter apps:
+    - For iOS, [Lux Light Meter Pro](https://itunes.apple.com/us/app/lux-light-meter-pro/id1292598866?mt=8)
+    - [Full list](https://www.photoworkout.com/best-light-meter-apps/)
+
+### Reference
+This simple luxmeter is based on:
+
+[Williams, David. “Design a Luxmeter Using a Light Dependent Resistor - Projects.” All About Circuits, 15 Dec. 2015, www.allaboutcircuits.com/projects/design-a-luxmeter-using-a-light-dependent-resistor/. ](www.allaboutcircuits.com/projects/design-a-luxmeter-using-a-light-dependent-resistor/)
--- tools/fit_lux_lr.py
+++ tools/fit_lux_lr.py
@@ -4,23 +4,52 @@ import argparse
 import numpy as np
 from scipy.optimize import lsq_linear
 
+def rmse(pred: np.ndarray, target: np.ndarray) -> float:
+    return np.sqrt(np.mean((pred-target)**2))
+
+def lux(logr: np.ndarray, m: float, b: float) -> np.ndarray:
+    return 10**(m * logr + b)
+
 
 parser = argparse.ArgumentParser(description='Fits least-squares model to lux-resistance data')
-parser.add_argument('-I', required=True, help='File with recorded data')
-parser.add_argument('-O', required=True, help='Parameters path')
+parser.add_argument('--input', required=True, help='File with recorded data')
+parser.add_argument('--output', required=True, help='Parameters path')
+parser.add_argument('--robust', action='store_true', help='Robust fit using Huber Regression')
 
 args = parser.parse_args()
 
-data = np.loadtxt(args.I, dtype=float, delimiter=',')
-Y = np.log10(data[:,0])
+if args.robust:
+    from sklearn import linear_model
 
-inp = np.log10(data[:,1]).reshape(-1, 1)
-ones = np.ones_like(inp)
+data = np.loadtxt(args.input, dtype=float, delimiter=',')
+target = data[:,1]
 
-x = np.concatenate([inp, ones], axis = 1)
-res = lsq_linear(x, Y)
-print('Optimization Report:')
-print(res)
-err = (inp**res.x[0]) * (10 ** res.x[1])
-print(f"Root Mean Squared Error \n{err}")
-np.savetxt(args.O, res.x)
+# Preprocess data
+Y = np.log10(target)
+inp = np.log10(data[:,0])
+
+print("")
+if args.robust:
+    print('Using huber regression ...')
+    x = inp.reshape(-1,1)
+    reg = linear_model.HuberRegressor()
+    reg.fit(x, Y)
+    m, b = reg.coef_[0], reg.intercept_
+    print(f"Number of outliers: {reg.outliers_.sum()}")
+    print(f"Number of iterations: {reg.n_iter_}")
+else:
+    print('Using least squares regression ...')
+    ones = np.ones_like(inp)
+    x = np.stack([inp, ones], axis = 1)
+    res = lsq_linear(x, Y)
+    print(res)
+    m, b = res.x[0], res.x[1]
+
+pred=lux(inp, m, b)
+err = rmse(pred, target)
+np.savetxt(args.output, [m, b])
+
+print("\n\n")
+print(f"Root Mean Squared Error: {err}")
+print(f"Lux Model parameters: {m}, {b}")
+print(f"Done!")
